edc4b757af2ca83753af96e452f76115d29b2f7f
= <=>
2023-04-13 16:25:05 +0200

    {feat: style} remove size for image

--- src/pages/RestaurantComponent.vue
+++ src/pages/RestaurantComponent.vue
@@ -43,13 +43,6 @@ export default {
         .wrapper-image{
             width: 100%;
             height: 50vh;
-    
-    
-            img{
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
         }
     }
     a{
